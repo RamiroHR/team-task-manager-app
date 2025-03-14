b2232cc73c7a51b43b666766aafa9788fa6be5e5
RamiroHR <19.rodriguez.89@gmail.com>
2025-03-14 21:35:48 +0100

    feat: add button in the UI to add a new task to the database and display it

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import TaskForm from "./taskForm"
 
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
 
+  const fetchTasks = async () => {
+    const res = await fetch('/api/tasks');
+    const data = await res.json();
+    setTasks(data);
+};
+
   useEffect(() => {
-    fetch('/api/tasks_hardcoded')      // Try the other endpoint /api/tasks_db
-      .then((res) => res.json())
-      .then((data) => setTasks(data));
+    fetchTasks();
   }, []);
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Tasks</h1>
+      <TaskForm onTaskAdded={fetchTasks} />
       <ul>
         {tasks.map((task) => (
           <li key={task.id} className="mb-2">
-            {task.title}
+            {task.id} | {task.title} --- (created: {task.createdAt.slice(0,10)} // updated: {task.updatedAt.slice(0,10)} )
           </li>
         ))}
       </ul>
     </div>
   );
 };
+
 
 export default TaskList;
