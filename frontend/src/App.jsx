8857ed0e84d74ea2321ff7e7b38773f982758def
RamiroHR <19.rodriguez.89@gmail.com>
2025-04-02 20:45:35 +0200

    fix: truncate long task titles, fix columns width & align col titles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { StrictMode, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import './index.css';
+/*import './index.css';*/
 import Header from './components/Header';
 import TaskList from './components/TaskList';
 import Login from './components/Login';
@@ -31,8 +31,8 @@ const App = () => {
 
   return (
     <Router>
-      <div className="max-w-7xl mx-auto p-4 sm:max-w-full md:max-w-7xl">
-        <Header isAuthenticated={isAuthenticated} onLogout={handleLogout} />
+      <div className="max-w-4xl mx-auto p-4 sm:max-w-2xl md:max-w-4xl lg:max-w-4xl">
+        <Header isAuthenticated={isAuthenticated} onLogout={handleLogout}/>
         <Routes>
           <Route
             path="/"
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import TaskForm from "./taskForm"
-// import Modal from './Modal';
 import TaskDetails from './TaskDetails';
 
 const TaskList = () => {
@@ -94,30 +93,31 @@ const TaskList = () => {
 
   return (
     <div className="p-4">
-      <TaskForm onTaskAdded={fetchTasks} />
-      <div className="mt-6 overflow-x-auto">
 
+      <TaskForm onTaskAdded={fetchTasks} />
+
+      <div className="mt-6 overflow-x-auto">
         {/* Table Headers */}
-        <div className="flex font-bold border-b-2 border-gray-200 pb-2 min-w-[1200px]">
-          <div className="w-1/24">ID</div>
-          <div className="w-1/15">Status</div>
-          <div className="w-4/12">Task</div>
-          <div className="w-2/20">Actions</div>
-          <div className="w-1/12">Created</div>
-          <div className="w-1/12">Updated</div>
+        <div className="flex font-bold border-b-3 border-gray-200 pb-2">
+          <div className="w-10">ID</div>
+          <div className="w-20">Status</div>
+          <div className="flex-1">Task</div>
+          <div className="w-24 text-center">Actions</div>
+          <div className="w-24 text-center">Created</div>
+          <div className="w-24 text-center">Updated</div>
         </div>
 
         {/* Task List */}
-        <ul className="min-w-[1200px]">
+        <ul>
           {tasks.map((task) => (
             <li
               key={task.id}
               className="flex items-center border-b border-gray-200 py-2"
             >
-              <div className="w-1/24">{task.id}</div>
-              <div className="w-1/15">{task.completed?'done 🟢' : 'todo ⚫'}</div>
-              <div className="w-4/12">{task.title.slice(0,50)}</div>
-              <div className="w-2/20 flex space-x-2">
+              <div className="w-10">{task.id}</div>
+              <div className="w-20">{task.completed?'done 🟢' : 'todo ⚫'}</div>
+              <div className="flex-1 truncate break-all">{task.title.slice(0,50)}</div>
+              <div className="w-24 flex justify-center space-x-2">
                 <button
                   onClick={() => handleSee(task)}
                   className="px-2 py-1 text-sm bg-blue-500 text-white rounded hover:bg-blue-600"
@@ -131,8 +131,8 @@ const TaskList = () => {
                   Del
                 </button>
               </div>
-              <div className="w-1/12">{task.createdAt.slice(0, 10)}</div>
-              <div className="w-1/12">{task.updatedAt.slice(0, 10)}</div>
+              <div className="w-24 text-center">{task.createdAt.slice(0, 10)}</div>
+              <div className="w-24 text-center">{task.updatedAt.slice(0, 10)}</div>
             </li>
           ))}
         </ul>
