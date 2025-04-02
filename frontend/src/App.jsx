2193593c4e2a5f741adb638c45463a2e7cf4a62b
RamiroHR <19.rodriguez.89@gmail.com>
2025-04-02 21:08:57 +0200

    fix: Make the Header match the login/register form width or keep normal with after login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { StrictMode, useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-/*import './index.css';*/
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useLocation }  from 'react-router-dom';
 import Header from './components/Header';
 import TaskList from './components/TaskList';
 import Login from './components/Login';
 import Register from './components/Register';
 
 const App = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
@@ -30,25 +33,27 @@ const App = () => {
   };
 
   return (
-    <Router>
-      <div className="max-w-4xl mx-auto p-4 sm:max-w-2xl md:max-w-4xl lg:max-w-4xl">
-        <Header isAuthenticated={isAuthenticated} onLogout={handleLogout}/>
-        <Routes>
-          <Route
-            path="/"
-            element={isAuthenticated ? <TaskList /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/login"
-            element={<Login onLogin={handleLogin} />}
-          />
-          <Route
-            path="/register"
-            element={<Register onRegister={handleRegister} />}
-          />
-        </Routes>
-      </div>
-    </Router>
+    <div className="max-w-4xl mx-auto p-4 sm:max-w-2xl md:max-w-4xl lg:max-w-4xl">
+      <Header
+        isAuthenticated={isAuthenticated}
+        onLogout={handleLogout}
+        isLoginPage={isLoginPage}
+      />
+      <Routes>
+        <Route
+          path="/"
+          element={isAuthenticated ? <TaskList /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={<Login onLogin={handleLogin} />}
+        />
+        <Route
+          path="/register"
+          element={<Register onRegister={handleRegister} />}
+        />
+      </Routes>
+    </div>
   );
 };
 
