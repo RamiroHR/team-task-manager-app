9720583c2a5798575613073c0a7b32305369ecb9
RamiroHR <19.rodriguez.89@gmail.com>
2025-04-04 22:52:02 +0200

    feat: add a 'discarded' field on task database to tag deleted tasks & update endpoints

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -11,107 +11,6 @@ const router = express.Router();
 router.use(authenticate);
 
 
-// GET TOP N MOST RECENT TASKS
-router.get('/tasks', async (req, res) => {
-
-  const N = 100;
-
-  try {
-    const tasks = await prisma.task.findMany({
-      orderBy: { createdAt: 'asc' },
-      take: N,
-    });
-    return res.json(tasks);
-
-  } catch (err) {
-    console.error('Error fetching tasks:', err);
-    res.status(500).json({
-      error: 'Internal server Error',
-      message: 'Failed to tefch tasks'
-    });
-  }
-});
-
-
-// GET RECENT TASKS in PAGE p
-router.get('/tasks/page/:p', async (req, res) => {
-  const { p } = req.params;
-  const pageSize = 10;
-
-  try {
-    const tasks = await prisma.task.findMany({
-      orderBy: { createdAt: 'asc'},
-      skip: (p-1)*pageSize,
-      take: pageSize
-    });
-    return res.json(tasks);
-
-  } catch (err) {
-    console.error(`Error fetching page ${p}`, err);
-    res.status(500).json({
-      error: 'Server Error',
-      message: 'Failed to fetch page'
-    })
-  }
-});
-
-
-// GET A TASK BY id
-router.get('/task/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const retrievedTask = await prisma.task.findUnique({
-      where: { id: Number(id) },
-    });
-
-    if (!retrievedTask) {
-      return res.status(404).json({
-        error: 'Not found',
-        message: 'Task not found'
-      })
-    }
-
-    return res.status(200).json(retrievedTask)
-
-  } catch (err) {
-    console.error(`Error fetching task:`, err)
-    res.status(500).json({
-      error: 'Internal server error',
-      message: 'Failed to fetch task'
-    })
-  }
-
-})
-
-
-// DELETE A TASK BY id
-router.delete('/task/delete/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const deletedTask = await prisma.task.delete({
-      where: { id: Number(id) },
-    });
-    return res.status(204).end();  //sucess but no response body
-
-  } catch(error) {
-    if (error.code === 'P2025') {
-      res.status(404).json({
-        error: 'Not found',
-        message: 'Task not found'
-      });
-    } else {
-      console.error('Error deleting task:', error)
-      res.status(500).json({
-        error: 'Internal Error server',
-        message: 'Failed to delete task'
-      });
-    }
-  }
-})
-
-
 // ADD A NEW TASK
 router.post('/task/new', async (req, res) => {
 
@@ -148,6 +47,114 @@ router.post('/task/new', async (req, res) => {
 
 })
 
+
+// GET TOP N MOST RECENT TASKS
+router.get('/tasks', async (req, res) => {
+
+  const N = 100;
+
+  try {
+    const tasks = await prisma.task.findMany({
+      where: { discarded: false},
+      orderBy: { createdAt: 'asc' },
+      take: N,
+    });
+    return res.json(tasks);
+
+  } catch (err) {
+    console.error('Error fetching tasks:', err);
+    res.status(500).json({
+      error: 'Internal server Error',
+      message: 'Failed to tefch tasks'
+    });
+  }
+});
+
+
+// GET RECENT TASKS in PAGE p
+router.get('/tasks/page/:p', async (req, res) => {
+  const { p } = req.params;
+  const pageSize = 10;
+
+  try {
+    const tasks = await prisma.task.findMany({
+      where: { discarded: false},
+      orderBy: { createdAt: 'asc'},
+      skip: (p-1)*pageSize,
+      take: pageSize
+    });
+    return res.json(tasks);
+
+  } catch (err) {
+    console.error(`Error fetching page ${p}`, err);
+    res.status(500).json({
+      error: 'Server Error',
+      message: 'Failed to fetch page'
+    })
+  }
+});
+
+
+// GET A TASK BY id
+router.get('/task/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const retrievedTask = await prisma.task.findUnique({
+      where: {
+        id: Number(id),
+        discarded: false
+       },
+    });
+
+    if (!retrievedTask) {
+      return res.status(404).json({
+        error: 'Not found',
+        message: 'Task not found'
+      })
+    }
+
+    return res.status(200).json(retrievedTask)
+
+  } catch (err) {
+    console.error(`Error fetching task:`, err)
+    res.status(500).json({
+      error: 'Internal server error',
+      message: 'Failed to fetch task'
+    })
+  }
+
+})
+
+
+// DELETE A TASK BY id
+router.put('/task/delete/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedTask = await prisma.task.update({
+      where: { id: Number(id) },
+      data: {discarded: true}
+    });
+
+    return res.json(deletedTask)
+    // return res.status(204).end();  //sucess but no response body
+
+  } catch(error) {
+    if (error.code === 'P2025') {
+      res.status(404).json({
+        error: 'Not found',
+        message: 'Task not found'
+      });
+    } else {
+      console.error('Error deleting task:', error)
+      res.status(500).json({
+        error: 'Internal Error server',
+        message: 'Failed to delete task'
+      });
+    }
+  }
+})
 
 
 // EDIT A TASK BY id
@@ -195,5 +202,27 @@ router.put('/task/edit/:id', async (req, res) => {
   }
 })
 
+// GET DISCARDED TASKS in PAGE p
+router.get('/tasks/discarded/page/:p', async (req, res) => {
+  const { p } = req.params;
+  const pageSize = 10;
+
+  try {
+    const tasks = await prisma.task.findMany({
+      where: { discarded: true },     // find discarded tasks
+      orderBy: { updatedAt: 'desc'},  // Show most recently discarded first
+      skip: (p-1)*pageSize,
+      take: pageSize
+    });
+    return res.json(tasks);
+
+  } catch (err) {
+    console.error(`Error fetching discarded tasks page ${p}`, err);
+    res.status(500).json({
+      error: 'Server Error',
+      message: 'Failed to fetch discarded tasks'
+    })
+  }
+});
 
 module.exports = router;
