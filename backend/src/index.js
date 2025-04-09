6358b89488bb24725270b3d3cd4d38ba6b045dad
RamiroHR <19.rodriguez.89@gmail.com>
2025-04-09 19:35:23 +0200

    fix: export server listening in PORT for Render deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,15 +15,8 @@ app.use('/api', taskRoutes);
 
 
 // Server startup logic
-let server;
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5000;
-  server = app.listen(PORT, () =>
-    console.log(`Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
-// Always export app, conditionally export server
-module.exports = process.env.NODE_ENV === 'production'
-  ? { app }
-  : { app, server };
+// Export app and server
+module.exports = { app, server };
