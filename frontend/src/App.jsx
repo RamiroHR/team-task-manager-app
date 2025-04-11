7ce02aef39934f93c390454fb26acdf62600f041
RamiroHR <19.rodriguez.89@gmail.com>
2025-04-11 19:14:03 +0200

    chore: set LoadingSpinner as separate component (reusability)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useContext } from 'react'
 import { AuthContext } from './context/AuthContext';
+import LoadingSpinner from './components/LoadingSpinner';
 import Header from './components/Header';
 import TaskList from './components/TaskList';
 import Login from './components/Login';
@@ -10,18 +11,10 @@ const App = () => {
   // State variables
   const { isAuthenticated, isLoading } = useContext(AuthContext);
 
-
-  // Don't render anything while verifying token (inside useEffect)
-  const LoadingSpinner = () => (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-    </div>
-  );
-
+  // Don't render anything while verifying token (inside AuthContext)
   if (isLoading) {
     return <LoadingSpinner />
   }
-
 
   // Render Main app only after isLoading is false
   return (
